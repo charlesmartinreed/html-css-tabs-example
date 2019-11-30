6fe5d77fcbb6f6b5bcb5c83b9f0cb1d5dc5f0c59
charlesmartinreed <charlesmartinreed@icloud.com>
2019-11-29 23:29:26 -0600

    Add js code to add/remove active classes. Add basic styling.

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ tabs.forEach(tab => {
     //   when we click, display that particular tab by grabbing the element via the tab target attribute we added before
     const target = document.querySelector(tab.dataset.tabTarget);
 
+    // clear out the old tabs
     tabContents.forEach(content => {
       content.classList.remove("active");
     });
 
+    tabs.forEach(tab => {
+      tab.classList.remove("active");
+    });
+
+    // add active to the new target/tag
+    tab.classList.add("active");
     target.classList.add("active");
   });
 });
