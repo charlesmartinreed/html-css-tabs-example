2ad49ae71e7aafc71fcfe46b740781df2c0400a6
charlesmartinreed <charlesmartinreed@icloud.com>
2019-11-29 22:51:58 -0600

    Remove active class from old item when new item clicked.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
 
 tabs.forEach(tab => {
   tab.addEventListener("click", () => {
     //   when we click, display that particular tab by grabbing the element via the tab target attribute we added before
     const target = document.querySelector(tab.dataset.tabTarget);
+
+    tabContents.forEach(content => {
+      content.classList.remove("active");
+    });
 
     target.classList.add("active");
   });
